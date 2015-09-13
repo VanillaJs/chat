32b2d11e6d9de639a4ba72fc81df3427e428adc4
rSamokhin <poshliemail@googlemail.com>
2015-09-14 00:27:29 +0300

    deploy script fix #13

--- tools/checkIfNewRevision.sh
+++ tools/checkIfNewRevision.sh
@@ -1,1 +1,1 @@
-cd .. && git rev-parse HEAD > ../.githead && git pull && [ `cat ../.githead` != `git rev-parse HEAD` ]
+git rev-parse HEAD > /tmp/.githead && git pull && [ `cat /tmp/.githead` != `git rev-parse HEAD` ]
