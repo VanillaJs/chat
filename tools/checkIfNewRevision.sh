be0143c08823e0c83cfe027ceaf35c0b5cd16b07
RSamokhin <poshliemail@googlemail.com>
2015-09-15 02:13:58 +0300

    fix

--- tools/checkIfNewRevision.sh
+++ tools/checkIfNewRevision.sh
@@ -1,1 +1,5 @@
-git rev-parse HEAD > /tmp/.githead && git pull && ( [ `cat /tmp/.githead` != `git rev-parse HEAD` ] || [ -z $(pgrep 'node') ] )
+#!/usr/bin/env bash
+
+git rev-parse HEAD > /tmp/.githead
+git pull
+ [[ `cat /tmp/.githead` != `git rev-parse HEAD` ]] || [[ -z $(pgrep "node") ]]
--- tools/reBuildProject.sh
+++ tools/reBuildProject.sh
@@ -1,12 +1,15 @@
+#!/usr/bin/env bash
+
 export PATH="/brew/bin:$PATH"
 export MANPATH="/brew/share/man:$MANPATH"
 export INFOPATH="/brew/share/info:$INFOPATH"
-cd ../server &&
-npm install &&
-cd ../web &&
-npm install &&
-cd ../web &&
-rm -rf build &&
-gulp build &&
-ps -ax | grep 'node app.js\|node server.js' | grep -v grep | awk '{print $1}' | xargs kill -9 &&
+
+cd ../server
+npm install 
+cd ../web 
+npm install
+cd ../web
+rm -rf build
+gulp build
+ps -ax | grep 'node' | grep -v grep | awk '{print $1}' | xargs kill -9
 cd ../server && node app.js
