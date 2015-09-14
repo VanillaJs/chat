6ef2123635bb8c83becf9daf93c728166a3cd749
rSamokhin <poshliemail@googlemail.com>
2015-09-14 14:16:39 +0300

    startup fix

--- tools/checkIfNewRevision.sh
+++ tools/checkIfNewRevision.sh
@@ -1,1 +1,1 @@
-git rev-parse HEAD > /tmp/.githead && git pull && ( [ `cat /tmp/.githead` != `git rev-parse HEAD` ] || [[ -z $(pgrep 'node') ]] )
+git rev-parse HEAD > /tmp/.githead && git pull && ( [ `cat /tmp/.githead` != `git rev-parse HEAD` ] || [ -z $(pgrep 'node') ] )
