e422960306851ef61119e1de94dae835777c7baf
emelyanovtv <emelyanovtv@gmail.com>
2015-09-19 13:59:00 +0300

    изменил jshint - глобальный на bin из node_modules

--- server/jshint-install.sh
+++ server/jshint-install.sh
@@ -1,7 +1,8 @@
+JSHINT=./node_modules/.bin/jshint
 npm install jshint --save &&
-jshint --config ../.jshintrc &&
+$JSHINT --config ../.jshintrc &&
 cd .. &&
 if [ ! -f ./.git/pre-commit ];
 then
     cp ./pre-commit.sample ./.git/pre-commit
-fi 
+fi
