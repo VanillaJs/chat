ffa45a898a7a0ad6a694d775037fa1dd803d185e
emelyanovtv <emelyanovtv@gmail.com>
2015-09-21 17:51:22 +0300

    обновил jshint-install впапке server

--- server/jshint-install.sh
+++ server/jshint-install.sh
@@ -1,4 +1,8 @@
-npm install jshint --save  &&
-jshint --config ../jshinrc &&
-cd ../
-
+JSHINT=./node_modules/.bin/jshint
+npm install jshint --save &&
+$JSHINT --config ../.jshintrc &&
+cd .. &&
+if [ ! -f ./.git/pre-commit ];
+then
+    cp ./pre-commit.sample ./.git/pre-commit
+fi
