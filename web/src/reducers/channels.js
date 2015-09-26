96b2e730d50ab33b69405415c85c943124b4692c
effrenus <effrenus@gmail.com>
2015-09-26 15:51:22 +0300

    Добавлен компонент channel

--- web/src/reducers/channels.js
+++ web/src/reducers/channels.js
@@ -2,19 +2,19 @@ import assign from 'object-assign';
 import channelActionType from '../constants/channels';
 import userActionType from '../constants/user';
 
-export function channels(state = {current:null, contacts:{}}, action) {
+export function channels(state = {current: null, contacts: {}}, action) {
 	switch (action.type) {
 	case channelActionType.SET_ACTIVE_CHANNEL:
 		return assign({}, state, {current: action.id});
 	case channelActionType.ADD_CHANNEL:
-		if(action.contact != null)
+		if (action.contact !== null)
 		{
 			state.contacts[action.contact._id] = action.contact;
 			return assign({}, state, {current: state.contacts});
 		}
 		return state;
 	case channelActionType.CHANNEL_REMOVE:
-		if(action.is_delete) {
+		if (action.is_delete) {
 			delete state.contacts[action.id];
 			return assign({}, state, {contacts: state.contacts});
 		}
