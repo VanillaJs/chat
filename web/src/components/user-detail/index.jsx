9e90a08be8326fd9d5ae9e0308173751b5bbe9db
Peter Neonov <petr.neonov@gmail.com>
2015-09-27 22:22:57 +0300

    style components

--- web/src/components/user-detail/index.jsx
+++ web/src/components/user-detail/index.jsx
@@ -9,7 +9,7 @@ class UserDetail extends Component {
 			<p className="user-detail__item"><span className="user-detail__number user-detail__number-groups">1</span> groups</p>
 			<p className="user-detail__item"><span className="user-detail__number user-detail__number-online">5</span> online</p>
 			<p className="user-detail__item"><span className="user-detail__number user-detail__number-unread">31</span> unread</p>
-			<div className="user-detail__item"><a className="user-detail__menu-link" href="#">Меню</a></div>
+			<div className="user-detail__item user-detail__menu"><i className="fa fa-angle-down"></i></div>
 		</div>
 		);
 	}
--- web/src/components/user-info/index.jsx
+++ web/src/components/user-info/index.jsx
@@ -3,15 +3,18 @@ import UserPic from '../user-pic';
 import './userinfo.sass';
 
 class UserInfo extends Component {
+
 	render() {
 		return (
 		<div className="user-info">
-			<UserPic/>
+			<div className="user-info__image">
+				<UserPic/>
+			</div>
 			<p className="user-info__greeting">
 				Welcome <span className="user-info__name">Dmitry Medvedev</span>
 			</p>
-			<div className="user-settings">
-				<a className="user-settings__icon" href="#">Settings</a>
+			<div className="user-info__icon-settings">
+				<i className="fa fa-cog"></i>
 			</div>
 		</div>
 		);
