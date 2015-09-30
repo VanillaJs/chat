1b40f0c88145372dd9ed80745df0ced22cccce02
Anna Vasilyeva <vasileva.ann@gmail.com>
2015-09-30 19:15:03 +0300

    fix test files acc to linter , webpack url loader , some styles for header component

--- test/server/atstart.js
+++ test/server/atstart.js
@@ -1,19 +1,16 @@
-var assert = require("assert");
-var directoryServer = "./../../server/";
+var directoryServer = './../../server/';
 var mongoose = require('mongoose');
 var config = require(directoryServer + 'config');
 
 describe('mongoose', function() {
-	describe('connection to mongo', function () {
-		it('should coonect to mongo', function (done) {
-			if(mongoose.connection._readyState === 0)
-			{
-				//если _readyState === , то подключеня к базе нет
+	describe('connection to mongo', function() {
+		it('should coonect to mongo', function(done) {
+			if (mongoose.connection._readyState === 0) {
+				// если _readyState === , то подключеня к базе нет
 				mongoose.connect(config.get('mongoose:uri'), done);
 			} else {
 				done();
 			}
 		});
 	});
-
 });
