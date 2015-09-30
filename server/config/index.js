a0d6f37ae2e6a05292176bf8de6318efbb7d8e89
emelyanovtv <emelyanovtv@gmail.com>
2015-09-30 15:54:06 +0300

    маленькая правка

--- server/config/index.js
+++ server/config/index.js
@@ -1,6 +1,6 @@
 var nconf = require('nconf');
 var path = require('path');
-var ENV = process.env.NODE_ENV || '';
+var ENV = process.env.NODE_ENV || 'develop';
 nconf.argv()
     .env()
     .file({file: path.join(__dirname, ENV+'.config.json')});
