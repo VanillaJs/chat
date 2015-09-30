73db86b4bef47cc91a7ac7540a965edb4e14f7ad
emelyanovtv <emelyanovtv@gmail.com>
2015-09-30 20:44:32 +0300

    fix eslint

--- server/config/index.js
+++ server/config/index.js
@@ -3,6 +3,6 @@ var path = require('path');
 var ENV = process.env.NODE_ENV || 'develop';
 nconf.argv()
     .env()
-    .file({file: path.join(__dirname, ENV+'.config.json')});
+    .file({file: path.join(__dirname, ENV + '.config.json')});
 
 module.exports = nconf;
