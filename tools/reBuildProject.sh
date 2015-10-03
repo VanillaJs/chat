b8d5e3baaa706012c854b793c774b7579e4c2583
RSamokhin <poshliemail@googlemail.com>
2015-10-03 17:04:00 +0300

    fix deploy

--- tools/reBuildProject.sh
+++ tools/reBuildProject.sh
@@ -5,7 +5,7 @@ export MANPATH="/brew/share/man:$MANPATH"
 export INFOPATH="/brew/share/info:$INFOPATH"
 echo Begin building
 pwd
-npm install
+#npm install
 npm run clean
 npm run build
 ps -ax | grep 'mongod' | grep -v grep | awk '{print $1}' | xargs kill -9
