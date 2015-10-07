33367e52778e46e308e54a28ae1c2e9ccfc1b2dc
emelyanovtv <emelyanovtv@gmail.com>
2015-10-07 13:12:56 +0300

    Добавил возможность обработки входящих данных , теперь все могут в обрабочик определенного типа добавить фильтр с типом события

---
+++ server/socket/types/constants/channel.js
@@ -0,0 +1,8 @@
+var channel = {
+	DISCONNECT: 'disconnect',
+	DELETE_CHANNEL: 'c.channel.delete',
+	JOIN_CHANNEL: 'c.channel.join',
+	ADD_CHANNEL: 'c.channel.add'
+};
+
+module.exports = channel;
---
+++ server/socket/types/constants/user.js
@@ -0,0 +1,8 @@
+var user = {
+	READ_MESSAGE: 'c.user.read_message',
+	GET_DATA: 'c.user.get_data',
+	MESSAGE_BY_ROOM: 'c.user.get_message_by_room',
+	SEND_MESSAGE: 'c.user.send_message'
+};
+
+module.exports = user;
