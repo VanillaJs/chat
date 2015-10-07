e973aa6583ae4910d86d14961c6f1351d3f7ead5
Anna Vasilyeva <vasileva.ann@gmail.com>
2015-10-07 15:30:25 +0300

    issue #58: added animation to channel add form + changed settings link in header to logout

--- web/src/components/header/index.jsx
+++ web/src/components/header/index.jsx
@@ -5,8 +5,8 @@ class Header extends Component {
 	render() {
 		return (
 			<header className="header">
-				<a className="header__logo" href="#">Vanilla js</a>
-				<a className="header__settings-link" href="#">Settings</a>
+				<a className="header__logo" href="#">Vanilla chat</a>
+				<a className="header__logout-link" href="/logout">Logout</a>
 			</header>
 		);
 	}
