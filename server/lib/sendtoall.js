ce7a13209bfa4603ff341ca9b3012faf2438df5c
emelyanovtv <emelyanovtv@gmail.com>
2015-10-10 17:16:47 +0300

    fitchure global

--- server/lib/sendtoall.js
+++ server/lib/sendtoall.js
@@ -1,8 +1,9 @@
 function sendToAll(Users, event, data, id) {
 	Object.keys(Users).map(function(key) {
 		Users[key].soketData.map(function(socket) {
-			if(socket.handshake.user._id !== id)
-			socket.emit(event, data);
+			if (socket.handshake.user._id !== id) {
+				socket.emit(event, data);
+			}
 		});
 	});
 }
